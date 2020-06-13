db3ac2dae31b248f10f82997acf7bdf99629b896
AshliRankin <ashlirankin18@gmail.com>
2020-06-13 08:17:40 -0400

    Added an imageView to the controller.

---
+++ DrawSampleProject/DrawerDisplayViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DrawSampleProject
+//
+//  Created by Ashli Rankin on 6/13/20.
+//  Copyright © 2020 Ashli Rankin. All rights reserved.
+//
+
+import UIKit
+
+class DrawerDisplayViewController: UIViewController {
+    @IBOutlet private weak var objectDisplayImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
