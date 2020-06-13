2e44b81fc08657b0bd89448a44f0023e505d8435
AshliRankin <ashlirankin18@gmail.com>
2020-06-13 08:13:07 -0400

    ititial commit

---
+++ DrawSampleProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DrawSampleProject
+//
+//  Created by Ashli Rankin on 6/13/20.
+//  Copyright © 2020 Ashli Rankin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
